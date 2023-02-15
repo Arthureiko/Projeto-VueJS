f18334bb517cb46a8bfbe42932fbeb21c07c2a9a
Arthur Eiko <arthureiko@MacBook-Pro.local>
2023-02-15 15:29:19 -0300

    feat: fist commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hellow</h1>
+    <p>My name is: {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      nome: "Arthur",
+    };
+  },
+};
+</script>
