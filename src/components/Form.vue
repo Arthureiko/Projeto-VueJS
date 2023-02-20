1b72b452205d754a1dbaa904980bfe2cb78c31b0
Arthur Eiko <arthureiko@MacBook-Pro.Dlink>
2023-02-20 17:35:56 -0300

    feat: add

---
+++ src/components/Form.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Meu formúlario:</h2>
+    <form action="">
+      <div>
+        <InputText />
+      </div>
+      <div>
+        <InputText />
+      </div>
+      <div>
+        <Submit />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import InputText from "./form/InputText.vue";
+import Submit from "./form/Submit.vue";
+export default {
+  name: "Form",
+  components: { InputText, Submit },
+};
+</script>
---
+++ src/components/Info.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p v-if="!esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>Javascript</li>
+      <li>PHP</li>
+      <li>C++</li>
+    </ul>
+    <div>
+      <button @click="showEmail">{{ textButton }}</button>
+    </div>
+    <p v-show="mostrar_email">Mandar email para: {{ email }}</p>
+    <p>
+      Para acessar meu portifólio
+      <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a>
+    </p>
+    <Picture />
+  </div>
+</template>
+
+<script>
+import Picture from "./Picture.vue";
+
+export default {
+  name: "Info",
+  components: {
+    Picture,
+  },
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: false,
+      email: "arthureiko29@gmail.com",
+      meu_link: "https://google.com",
+      textButton: "Mostrar email",
+    };
+  },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email;
+      if (!this.mostrar_email) {
+        this.textButton = "Mostrar email";
+      } else {
+        this.textButton = "Esconder email";
+      }
+    },
+  },
+};
+</script>
